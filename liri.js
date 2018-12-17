05ee430b0436799cd716f5549a92fedb04bbd1b8
Christian <chansen302@gmail.com>
2018-12-16 22:25:40 -0800

    All code working so far, though Spotify output still isn't formatted.  Read from text file works too. Used inquirer prompts to select user command and a second prompt to type in their search instead of using arguments.

---
+++ liri.js
@@ -0,0 +1,109 @@
+require("dotenv").config(); // Set up dotenv for key storage purposes
+
+var moment = require("moment");
+var Spotify = require("node-spotify-api");
+var inquirer = require("inquirer"); // Set up inquirer for user input
+var request = require("request"); // Set up request for most API operations (not Spotify)
+var fs = require("fs"); // Include the node file system (no install required)
+
+var keys = require("./keys"); // Get the export object from "keys.js" and store in variable
+
+// Takes the spotify object imported from "keys.js" and inserts it as a parameter to a new node-spotify-api instance
+var spotify = new Spotify(keys.spotify);
+
+// Creates a new object instances to hold api object exports from 'keys.js'
+var omdb = keys.omdb;
+var bands = keys.bandsInTown;
+
+// Function to parse text file for command
+function parseTextandRun() {
+    var APIchoice;
+    var query;
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            console.log("Error:", error)
+        }
+        {
+            var output = data.split(","); // Split string into 2 arrays separated by comma
+            APIchoice = output[0]; // Should be string for search type
+            query = output[1]; // String for search
+            runSearch(APIchoice, query);
+        }
+    })
+}
+
+function runSearch(choice, query) {
+    console.log(choice);
+    switch(choice) { // Done since search mechanism is not uniform across API's
+            case "spotify-this-song":
+                spotify.search({type: 'track', query: query}, function(error, data) {
+                    if (!error) {
+                        console.log(data); // Will reformat depending on initial output
+                    }
+                })
+                break;
+            case "concert-this":
+                request("https://rest.bandsintown.com/artists/" + query.search + "/events?app_id=" + bands.key, function(error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        var concert = JSON.parse(body);
+                        var date = moment(concert[0].venue.datetime); // Store date in moment object to change output format
+                        console.log("Name of Venue:", concert[0].venue.name);
+                        console.log("Venue Location: " + concert[0].venue.city + ", " + concert[0].venue.region + ", " + concert[0].venue.country);
+                        console.log("Date of Event:", date.format("MM/DD/YYYY"));
+                    }
+                    else  {
+                        console.log("Error:", error);
+                    }
+                })
+                break;
+            case "movie-this":
+                request("http://www.omdbapi.com/?t=" + query.search + "&y=&plot=short&apikey=" + omdb.key, function(error, response, body) {
+                    if (!error && response.statusCode === 200) {
+                        var movie = JSON.parse(body);
+                        console.log("Movie Title:", movie.Title);
+                        console.log("Year of Release:", movie.Year)
+                        console.log("Rating on IMDB:", movie.imdbRating);
+                        console.log("Plot:", movie.Plot);
+                    }
+                    else {
+                        console.log("Error:", error);
+                    }
+                })
+                break;
+    }
+}
+
+// Function to run the requested type of search
+function search(APIchoice) {
+    console.log(APIchoice);
+    inquirer.prompt({
+        type: "input",
+        name: "search",
+        message: "Type in what you would like to search: "
+    }).then(function(query) {
+        runSearch(APIchoice, query);
+    })
+}
+
+// Code for getting user command input (will use list form)
+inquirer.prompt({
+    name: "choice",
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["Search for a concert by artist", "Search Spotify", "Search for a movie", "Get command from file"]
+}).then(function(command) {
+    switch (command.choice) {
+        case "Search for a concert by artist":
+            search("concert-this");
+            break;
+        case "Search Spotify":
+            search("spotify-this-song");
+            break;
+        case "Search for a movie":
+            search("movie-this");
+            break;
+        case "Get command from file":
+            parseTextandRun();
+            break;
+    }
+})
